a00d3e71c0d5b30582ad385c2278138c646166e5
Ed Maste <emaste@freebsd.org>
2018-03-15 22:29:38 -0400

    Switch to external retpoline thunks
    Upstream:	after previous commit (adding thunks)

--- sys/conf/kern.mk
+++ sys/conf/kern.mk
@@ -246,7 +246,7 @@ CFLAGS+=	-fstack-protector-strong
 #
 .if defined(COMPILER_FEATURES) && ${COMPILER_FEATURES:Mretpoline} != "" && \
     ${MK_KERNEL_RETPOLINE} != "no"
-CFLAGS+=	-mretpoline
+CFLAGS+=	-mretpoline-external-thunk
 .endif
 
 #
