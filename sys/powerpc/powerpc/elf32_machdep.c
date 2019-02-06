8aaa4742b0701f5d8dd8dc276e4fbed98c61ad36
Ed Maste <emaste@freebsd.org>
2019-02-06 12:13:16 -0500

    default to non-executable stack
    Very old software may have required an executable stack but I suspect
    all relevant instances (on amd64 at least) have been fixed long ago.
    
    - kernel defaults (amd64,arm,i386,mips,powerpc)
    - libc: thread stacks in statically linked binaries
    
    See also PR 252549
    
    Upstream:	as part of holistic investigation

--- sys/powerpc/powerpc/elf32_machdep.c
+++ sys/powerpc/powerpc/elf32_machdep.c
@@ -104,7 +104,7 @@ struct sysentvec elf32_freebsd_sysvec = {
 	.sv_imgact_try	= NULL,
 	.sv_minsigstksz	= MINSIGSTKSZ,
 	.sv_minuser	= VM_MIN_ADDRESS,
-	.sv_stackprot	= VM_PROT_ALL,
+	.sv_stackprot	= VM_PROT_READ | VM_PROT_WRITE,
 #ifdef __powerpc64__
 	.sv_maxuser	= VM_MAXUSER_ADDRESS32,
 	.sv_usrstack	= FREEBSD32_USRSTACK,
