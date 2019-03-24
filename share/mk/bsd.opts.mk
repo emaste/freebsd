8d5395b48a6891b587d0aa416a775f50bd4ac50c
Ed Maste <emaste@freebsd.org>
2019-03-24 18:12:09 -0400

    Enable WITH_BIND_NOW by default
    Upstream:	benchmark; discuss on ML with results and ABI concerns

--- share/mk/bsd.opts.mk
+++ share/mk/bsd.opts.mk
@@ -50,6 +50,7 @@ __<bsd.opts.mk>__:
 
 __DEFAULT_YES_OPTIONS = \
     ASSERT_DEBUG \
+    BIND_NOW \
     DEBUG_FILES \
     DOCCOMPRESS \
     INCLUDES \
@@ -69,7 +70,6 @@ __DEFAULT_YES_OPTIONS = \
 
 __DEFAULT_NO_OPTIONS = \
     ASAN \
-    BIND_NOW \
     CCACHE_BUILD \
     CTF \
     INIT_ALL_PATTERN \
