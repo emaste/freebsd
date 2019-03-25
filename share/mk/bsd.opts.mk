4c0195f87ac2a98ee8002d2d726ce2369af9d698
Ed Maste <emaste@freebsd.org>
2019-03-25 08:57:36 -0400

    Enable WITH_RETPOLINE by default
    Upstream:	benchmark; as part of holistic build option work

--- share/mk/bsd.opts.mk
+++ share/mk/bsd.opts.mk
@@ -65,6 +65,7 @@ __DEFAULT_YES_OPTIONS = \
     NIS \
     NLS \
     OPENSSH \
+    RETPOLINE \
     SSP \
     TESTS \
     TOOLCHAIN \
@@ -80,7 +81,6 @@ __DEFAULT_NO_OPTIONS = \
     INSTALL_AS_USER \
     MANSPLITPKG \
     PROFILE \
-    RETPOLINE \
     STALE_STAGED \
     UBSAN
 
