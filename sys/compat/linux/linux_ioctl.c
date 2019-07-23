3a18f8c8802074b0d80fff4c4ce9b70f05e32dca
Ed Maste <emaste@freebsd.org>
2019-07-23 12:51:39 -0400

    linux: zero output struct in bsd_to_linux_termios
    Avoid potential structure padding leak and slightly simplify the
    routine.
    
    Upstream:	yes, verify not addressed elsewhere
    Reported by:	Vlad Tsyrklevich <vlad@tsyrklevich.net>
    Security:	Potential kernel memory disclosure

--- sys/compat/linux/linux_ioctl.c
+++ sys/compat/linux/linux_ioctl.c
@@ -417,7 +417,8 @@ bsd_to_linux_termios(struct termios *bios, struct linux_termios *lios)
 {
 	int i;
 
-	lios->c_iflag = 0;
+	memset(lios, 0, sizeof(*lios));
+
 	if (bios->c_iflag & IGNBRK)
 		lios->c_iflag |= LINUX_IGNBRK;
 	if (bios->c_iflag & BRKINT)
@@ -445,7 +446,6 @@ bsd_to_linux_termios(struct termios *bios, struct linux_termios *lios)
 	if (bios->c_iflag & IMAXBEL)
 		lios->c_iflag |= LINUX_IMAXBEL;
 
-	lios->c_oflag = 0;
 	if (bios->c_oflag & OPOST)
 		lios->c_oflag |= LINUX_OPOST;
 	if (bios->c_oflag & ONLCR)
@@ -470,7 +470,6 @@ bsd_to_linux_termios(struct termios *bios, struct linux_termios *lios)
 	if (bios->c_cflag & CRTSCTS)
 		lios->c_cflag |= LINUX_CRTSCTS;
 
-	lios->c_lflag = 0;
 	if (bios->c_lflag & ISIG)
 		lios->c_lflag |= LINUX_ISIG;
 	if (bios->c_lflag & ICANON)
@@ -526,7 +525,6 @@ bsd_to_linux_termios(struct termios *bios, struct linux_termios *lios)
 		    lios->c_cc[i] == _POSIX_VDISABLE)
 			lios->c_cc[i] = LINUX_POSIX_VDISABLE;
 	}
-	lios->c_line = 0;
 }
 
 static void
