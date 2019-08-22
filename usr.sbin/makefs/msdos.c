5e19f6bd9aa8836e1627f4e6549325f799f8ff89
Ed Maste <emaste@freebsd.org>
2019-08-22 17:11:44 -0400

    makefs: share direntry.h between kernel msdosfs and makefs
    For upstreaming, migrate to sharing msdosfs_conv first following the
    kernel's function prototypes.
    
    Upstream:	after resolving the msdosfs_conv differences

--- usr.sbin/makefs/msdos.c
+++ usr.sbin/makefs/msdos.c
@@ -61,7 +61,7 @@ __FBSDID("$FreeBSD$");
 
 #include <mkfs_msdos.h>
 #include <fs/msdosfs/bpb.h>
-#include "msdos/direntry.h"
+#include <fs/msdosfs/direntry.h>
 #include <fs/msdosfs/denode.h>
 #include <fs/msdosfs/msdosfsmount.h>
 
