1ea9316cccb4e7d11184d7bbdc06a8c14dfd63b1
Ed Maste <emaste@freebsd.org>
2019-09-11 09:45:06 -0400

    sys/dev/acpi_support: remove trailing whitespace
    Upstream:	yes

--- sys/dev/acpi_support/acpi_asus.c
+++ sys/dev/acpi_support/acpi_asus.c
@@ -484,7 +484,7 @@ static struct {
 	{
 		.name		= "camera",
 		.method		= ACPI_ASUS_METHOD_CAMERA,
-		.description	= "internal camera state",  
+		.description	= "internal camera state",
 	},
 	{
 		.name		= "cardreader",
@@ -622,7 +622,7 @@ good:
 			AcpiOsFree(Buf.Pointer);
 			return (rv);
 		}
-		
+
 		/*
 		 * Some models look exactly the same as other models, but have
 		 * their own ids.  If we spot these, set them up with the same
