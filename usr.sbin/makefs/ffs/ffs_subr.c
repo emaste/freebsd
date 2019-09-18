88c8c780af279d5c35855823643262581b107062
Ed Maste <emaste@freebsd.org>
2019-09-17 22:04:26 -0400

    makefs: strip trailing whitespace
    Upstream:	yes, but compare with NetBSD

--- usr.sbin/makefs/ffs/ffs_subr.c
+++ usr.sbin/makefs/ffs/ffs_subr.c
@@ -45,7 +45,7 @@ __FBSDID("$FreeBSD$");
 #include "ffs/ufs_bswap.h"
 
 /*
- * Update the frsum fields to reflect addition or deletion 
+ * Update the frsum fields to reflect addition or deletion
  * of some frags.
  */
 void
