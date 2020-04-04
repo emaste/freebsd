cf4f74da7fe7a1e63c1b1570f48d63eeff92ffc8
Ed Maste <emaste@freebsd.org>
2020-04-04 12:20:39 -0400

    Enable WITH_BSD_GREP by default
    Upstream:	encourage kevans

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -68,6 +68,7 @@ __DEFAULT_YES_OPTIONS = \
     BOOTPARAMD \
     BOOTPD \
     BSD_CPIO \
+    BSD_GREP \
     BSDINSTALL \
     BSNMP \
     BZIP2 \
@@ -106,7 +107,6 @@ __DEFAULT_YES_OPTIONS = \
     GAMES \
     GDB \
     GNU_DIFF \
-    GNU_GREP \
     GOOGLETEST \
     GPIO \
     HAST \
@@ -200,10 +200,10 @@ __DEFAULT_YES_OPTIONS = \
 __DEFAULT_NO_OPTIONS = \
     BEARSSL \
     BHYVE_SNAPSHOT \
-    BSD_GREP \
     CLANG_EXTRAS \
     DTRACE_TESTS \
     EXPERIMENTAL \
+    GNU_GREP \
     GNU_GREP_COMPAT \
     HESIOD \
     LIBSOFT \
