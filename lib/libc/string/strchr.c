387e934798f88da9c716c6e6230917d0ac3ce426
Ed Maste <emaste@FreeBSD.org>
2020-09-07 20:50:38 -0400

    clang-format libc string functions imported from musl
    We have adopted these and don't consider them 'contrib' code.
    Followon to D17630.

--- lib/libc/string/strchr.c
+++ lib/libc/string/strchr.c
@@ -29,7 +29,8 @@ __FBSDID("$FreeBSD$");
 
 char *__strchrnul(const char *, int);
 
-char *strchr(const char *s, int c)
+char *
+strchr(const char *s, int c)
 {
 	char *r = __strchrnul(s, c);
 	return *(unsigned char *)r == (unsigned char)c ? r : 0;
