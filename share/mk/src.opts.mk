e7424301a0f83dac4afe05024be4e975a34ac314
Ed Maste <emaste@FreeBSD.org>
2020-12-26 11:35:42 -0500

    Disable GNU_DIFF, SENDMAIL, TCP_WRAPPERS by default

--- share/mk/src.opts.mk
+++ share/mk/src.opts.mk
@@ -108,7 +108,6 @@ __DEFAULT_YES_OPTIONS = \
     FTP \
     GAMES \
     GH_BC \
-    GNU_DIFF \
     GOOGLETEST \
     GPIO \
     HAST \
@@ -168,7 +167,6 @@ __DEFAULT_YES_OPTIONS = \
     REPRODUCIBLE_BUILD \
     RESCUE \
     ROUTED \
-    SENDMAIL \
     SERVICESDB \
     SETUID_LOGIN \
     SHARED_TOOLCHAIN \
@@ -181,7 +179,6 @@ __DEFAULT_YES_OPTIONS = \
     SYSTEM_COMPILER \
     SYSTEM_LINKER \
     TALK \
-    TCP_WRAPPERS \
     TCSH \
     TELNET \
     TEXTPROC \
@@ -204,6 +201,7 @@ __DEFAULT_NO_OPTIONS = \
     DETECT_TZ_CHANGES \
     DTRACE_TESTS \
     EXPERIMENTAL \
+    GNU_DIFF \
     HESIOD \
     LOADER_FIREWIRE \
     LOADER_VERBOSE \
@@ -212,7 +210,9 @@ __DEFAULT_NO_OPTIONS = \
     OFED_EXTRA \
     OPENLDAP \
     RPCBIND_WARMSTART_SUPPORT \
+    SENDMAIL \
     SORT_THREADS \
+    TCP_WRAPPERS \
     ZONEINFO_LEAPSECONDS_SUPPORT \
 
 # LEFT/RIGHT. Left options which default to "yes" unless their corresponding
