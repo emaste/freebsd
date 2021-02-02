6c8562c2e26558e7e80cc34e2a7627d1ca594048
Ed Maste <emaste@FreeBSD.org>
2021-02-01 19:40:50 -0500

    XXX highlight D28445 cases
    XXX Need to double-check after resolving a conflict upon a rebase
    
    Conflicts:
    	sys/kern/vfs_lookup.c
    case 3 and 5 conflicted, revisit

--- sys/kern/vfs_lookup.c
+++ sys/kern/vfs_lookup.c
@@ -1253,6 +1253,7 @@ dirloop:
 	 *    file descriptor directory.
 	 */
 	if (cnp->cn_flags & ISDOTDOT) {
+		/* Case 1 */
 		if (__predict_false((ndp->ni_lcf & (NI_LCF_STRICTREL_KTR |
 		    NI_LCF_CAP_DOTDOT_KTR)) == NI_LCF_STRICTREL_KTR))
 			NI_CAP_VIOLATION(ndp, cnp->cn_pnbuf);
@@ -1261,6 +1262,7 @@ dirloop:
 			error = ENOTCAPABLE;
 			goto bad;
 		}
+		/* Case 2 */
 		if ((cnp->cn_flags & ISLASTCN) != 0 &&
 		    (cnp->cn_nameiop == DELETE || cnp->cn_nameiop == RENAME)) {
 			error = EINVAL;
@@ -1268,8 +1270,10 @@ dirloop:
 		}
 		for (;;) {
 			bool isroot;
+			/* For Case 5 */
 
 			isroot = vfs_lookup_isroot(ndp, dp);
+			/* Case 3 and 5 */
 			if (__predict_false(isroot && (ndp->ni_lcf &
 			    (NI_LCF_STRICTREL | NI_LCF_STRICTREL_KTR)) != 0)) {
 				if ((ndp->ni_lcf & NI_LCF_STRICTREL_KTR) != 0)
@@ -1286,6 +1290,7 @@ dirloop:
 				VREF(dp);
 				goto nextname;
 			}
+			/* Case 4 */
 			if ((dp->v_vflag & VV_ROOT) == 0)
 				break;
 			if (VN_IS_DOOMED(dp)) {	/* forced unmount */
@@ -1299,6 +1304,7 @@ dirloop:
 			vn_lock(dp,
 			    enforce_lkflags(dp->v_mount, cnp->cn_lkflags |
 			    LK_RETRY));
+			/* Case 6 */
 		}
 	}
 
