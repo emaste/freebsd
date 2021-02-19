7256a64f43aca6bcadb2456be501b9040a6f2e81
Ed Maste <emaste@FreeBSD.org>
2021-02-18 20:32:58 -0500

    bhyve error reporting patches
    PR:		250671

--- usr.sbin/bhyvectl/bhyvectl.c
+++ usr.sbin/bhyvectl/bhyvectl.c
@@ -1964,7 +1964,8 @@ main(int argc, char *argv[])
 	if (!error) {
 		ctx = vm_open(vmname);
 		if (ctx == NULL) {
-			printf("VM:%s is not created.\n", vmname);
+			printf("VM:%s could not be opened: %s\n", vmname,
+			    strerror(errno));
 			exit (1);
 		}
 	}
