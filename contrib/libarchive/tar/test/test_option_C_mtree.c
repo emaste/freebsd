3ee1aff788c151a4b2ef8749014bbd23be2bce27
Ed Maste <emaste@FreeBSD.org>
2021-05-22 10:36:52 -0400

    Remove "All Rights Reserved" from FreeBSD Foundation copyrights
    These ones were unambiguous cases where the Foundation was the only
    listed copyright holder.

--- contrib/libarchive/tar/test/test_option_C_mtree.c
+++ contrib/libarchive/tar/test/test_option_C_mtree.c
@@ -2,7 +2,6 @@
  * SPDX-License-Identifier: BSD-2-Clause
  *
  * Copyright (c) 2018 The FreeBSD Foundation
- * All rights reserved.
  *
  * This software was developed by Arshan Khanifar <arshankhanifar@gmail.com>
  * under sponsorship from the FreeBSD Foundation.
