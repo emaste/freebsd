4a91f4f333e9a6ed42ccd23f182a3a7c54aebf17
Ed Maste <emaste@FreeBSD.org>
2023-04-18 15:09:18 -0400

    Revert "mail: start cleaning clang warnings"
    This reverts commit 2214e5383073014c5df5c52d26f3345a8b76a54c.

--- usr.bin/mail/cmd3.c
+++ usr.bin/mail/cmd3.c
@@ -43,12 +43,11 @@
  * and forking a sh -c
  */
 int
-shell(void *arg)
+shell(void *str)
 {
 	sig_t sigint = signal(SIGINT, SIG_IGN);
 	char *sh;
 	char cmd[BUFSIZ];
-	char *str = arg;
 
 	if (strlcpy(cmd, str, sizeof(cmd)) >= sizeof(cmd))
 		return (1);
@@ -67,7 +66,7 @@ shell(void *arg)
  */
 /*ARGSUSED*/
 int
-dosh(void *arg __unused)
+dosh(void *str __unused)
 {
 	sig_t sigint = signal(SIGINT, SIG_IGN);
 	char *sh;
