9a95afa231b4d9b119d576d307bbe69feab08335
Ed Maste <emaste@FreeBSD.org>
2023-10-04 10:56:32 -0400

    Remove WITHOUT_CAPSICUM handling
    Capsicum is non-optional as of c24c117b9644 ("Remove
    WITHOUT_{CAPSICUM,CASPER} options").

--- usr.bin/bsdiff/bspatch/bspatch.c
+++ usr.bin/bsdiff/bspatch/bspatch.c
@@ -27,9 +27,7 @@
  */
 
 #include <sys/cdefs.h>
-#ifndef WITHOUT_CAPSICUM
 #include <sys/capsicum.h>
-#endif
 
 #include <bzlib.h>
 #include <err.h>
@@ -111,9 +109,7 @@ int main(int argc, char *argv[])
 	off_t oldpos, newpos;
 	off_t ctrl[3];
 	off_t i, lenread, offset;
-#ifndef WITHOUT_CAPSICUM
 	cap_rights_t rights_dir, rights_ro, rights_wr;
-#endif
 
 	if (argc != 4)
 		usage();
@@ -147,7 +143,6 @@ int main(int argc, char *argv[])
 		err(1, "open(%s)", argv[2]);
 	atexit(exit_cleanup);
 
-#ifndef WITHOUT_CAPSICUM
 	if (cap_enter() < 0)
 		err(1, "failed to enter security sandbox");
 
@@ -164,7 +159,6 @@ int main(int argc, char *argv[])
 	    cap_rights_limit(dirfd, &rights_dir) < 0)
 		err(1, "cap_rights_limit() failed, could not restrict"
 		    " capabilities");
-#endif
 
 	/*
 	File format:
