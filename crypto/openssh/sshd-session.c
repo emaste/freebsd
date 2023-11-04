041b5c2981dac86e94a33b371142781c9e0927c8
Ed Maste <emaste@FreeBSD.org>
2023-11-04 12:15:39 -0400

    blocklistd rename in sshd (part)
    Conflicts:
    	crypto/openssh/auth-pam.c
    	crypto/openssh/servconf.c
    	crypto/openssh/servconf.h
    	crypto/openssh/sshd.c
    
    Conflicts:
    	crypto/openssh/servconf.c
    	crypto/openssh/servconf.h
    	crypto/openssh/sshd.c

--- crypto/openssh/sshd-session.c
+++ crypto/openssh/sshd-session.c
@@ -109,7 +109,7 @@
 #include "sk-api.h"
 #include "srclimit.h"
 #include "dh.h"
-#include "blacklist_client.h"
+#include "blocklist_client.h"
 
 /* Re-exec fds */
 #define REEXEC_DEVCRYPTO_RESERVED_FD	(STDERR_FILENO + 1)
@@ -1317,7 +1317,7 @@ main(int ac, char **av)
 		fatal("sshbuf_new loginmsg failed");
 	auth_debug_reset();
 
-	BLACKLIST_INIT();
+	BLOCKLIST_INIT();
 
 	if (privsep_preauth(ssh) == 1)
 		goto authenticated;
