f5b06a13bb2a61e360e57623751d97da82b83eba
Ed Maste <emaste@FreeBSD.org>
2023-12-01 21:42:36 -0500

    sound: whitespace cleanup
    Conflicts:
    	sys/dev/sound/pci/solo.c
    	sys/dev/sound/pcm/channel.h
    
    Conflicts:
    	sys/dev/sound/pcm/channel.h
    
    Conflicts:
    	sys/dev/sound/pcm/dsp.c
    
    Conflicts:
    	sys/dev/sound/pcm/feeder_mixer.c
    	sys/dev/sound/pcm/pcm.h
    	sys/dev/sound/pcm/sound.c
    
    Conflicts:
    	sys/dev/sound/pcm/sound.c

--- sys/dev/sound/pcm/sndstat.c
+++ sys/dev/sound/pcm/sndstat.c
@@ -102,7 +102,7 @@ struct sndstat_file {
 	size_t devs_nbytes;	/* (l) */
 	TAILQ_HEAD(, sndstat_userdev) userdev_list;	/* (l) */
 	int out_offset;
-  	int in_offset;
+	int in_offset;
 	int fflags;
 };
 
@@ -1374,7 +1374,7 @@ sndstat_prepare(struct sndstat_file *pf_self)
 	struct sndstat_entry *ent;
 	struct snddev_info *d;
 	struct sndstat_file *pf;
-    	int k;
+	int k;
 
 	/* make sure buffer is reset */
 	sbuf_clear(s);
@@ -1431,7 +1431,7 @@ sndstat_prepare(struct sndstat_file *pf_self)
 		sbuf_printf(s, "No devices installed from userspace.\n");
 
 	sbuf_finish(s);
-    	return (sbuf_len(s));
+	return (sbuf_len(s));
 }
 
 static void
