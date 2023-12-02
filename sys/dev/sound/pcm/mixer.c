e30ff5b56f29cf841ff2368de12171b7ca2b776e
Ed Maste <emaste@FreeBSD.org>
2023-12-02 12:30:19 -0500

    sound: ws cleanup
    Conflicts:
    	sys/dev/sound/macio/aoa.c
    
    Conflicts:
    	sys/dev/sound/pcm/dsp.c
    
    Conflicts:
    	sys/dev/sound/midi/midi.c
    	sys/dev/sound/midi/synth_if.m
    
    Conflicts:
    	sys/dev/sound/pcm/mixer.c
    
    Conflicts:
    	sys/dev/sound/pcm/ac97.c
    
    Conflicts:
    	sys/dev/sound/usb/uaudio.c
    
    Conflicts:
    	sys/dev/sound/pcm/mixer.c

--- sys/dev/sound/pcm/mixer.c
+++ sys/dev/sound/pcm/mixer.c
@@ -414,7 +414,7 @@ mixer_get_recroute(struct snd_mixer *m, int *route)
  *
  * This function sets a recording source based on a recording device's
  * routing number.  Said number is translated to an old school recdev
- * mask and passed over mixer_setrecsrc. 
+ * mask and passed over mixer_setrecsrc.
  *
  * @param m	mixer context container thing
  *
