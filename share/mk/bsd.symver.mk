5814e750543a19aa0468aaad8fe0e7843b50289f
Ed Maste <emaste@FreeBSD.org>
2024-05-24 12:15:17 -0400

    bsd.symver.mk: exclude linker flags from $CFLAGS in $CPP invocation
    Suggested by brooks@ in D45346

--- share/mk/bsd.symver.mk
+++ share/mk/bsd.symver.mk
@@ -44,7 +44,7 @@ _vgen=  ${path}/${VERSION_GEN}
 # Run the symbol maps through the C preprocessor before passing
 # them to the symbol version generator.
 ${VERSION_MAP}: ${VERSION_DEF} ${_vgen} ${SYMBOL_MAPS}
-	cat ${SYMBOL_MAPS} | ${CPP} ${CFLAGS} - - \
+	cat ${SYMBOL_MAPS} | ${CPP} ${CFLAGS:N-Wl,*} - - \
 	    | awk -v vfile=${VERSION_DEF} -f ${_vgen} > ${.TARGET}
 .endif	# !empty(VERSION_DEF) && !empty(SYMBOL_MAPS)
 .endif  # !target(__<bsd.symver.mk>__)
