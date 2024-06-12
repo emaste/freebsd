302014f537002c1754d07b0952dc9e0b91ea9feb
Ed Maste <emaste@FreeBSD.org>
2024-06-12 19:48:24 -0400

    tzsetup: ask local/UTC question only on x86
    Storing local time in the RTC is a legacy of 1990s PCs; it's not
    relevant on other platforms of interest to FreeBSD.
    
    While here switch to C99 bool.
    
    Sponsored by:	The FreeBSD Foundation
    Reviewed by:	allanjude (earlier), imp (earlier)
    Differential Revision: https://reviews.freebsd.org/D45575

--- usr.sbin/tzsetup/tzsetup.c
+++ usr.sbin/tzsetup/tzsetup.c
@@ -40,6 +40,7 @@
 #include <sys/cdefs.h>
 #include <err.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -902,13 +903,23 @@ main(int argc, char **argv)
 	char		prompt[128];
 	int		fd;
 #endif
-	int		c, rv, skiputc;
+	int		c, rv;
+	bool		skiputc;
+	char		*dztpath;
+#if defined(__i386__) || defined(__amd64__)
 	char		vm_guest[16] = "";
 	size_t		len = sizeof(vm_guest);
-	char		*dztpath;
 
+	skiputc = false;
+
+	/* Default skiputc to true for VM guests */
+	if (sysctlbyname("kern.vm_guest", vm_guest, &len, NULL, 0) == 0 &&
+	    strcmp(vm_guest, "none") != 0)
+		skiputc = true;
+#else
+	skiputc = true;
+#endif
 	dztpath = NULL;
-	skiputc = 0;
 
 #ifdef HAVE_BSDDIALOG
 	setlocale(LC_ALL, "");
@@ -938,7 +949,7 @@ main(int argc, char **argv)
 #endif
 			break;
 		case 's':
-			skiputc = 1;
+			skiputc = true;
 			break;
 		default:
 			usage();
@@ -1033,7 +1044,7 @@ main(int argc, char **argv)
 	if (bsddialog_init() == BSDDIALOG_ERROR)
 		errx(1, "Error bsddialog: %s\n", bsddialog_geterror());
 
-	if (skiputc == 0) {
+	if (!skiputc) {
 		snprintf(prompt, sizeof(prompt),
 		    "Is this machine's CMOS clock set to UTC?  "
 		    "If it is set to local time,\n"
