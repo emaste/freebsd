7dc0c9c3e9d52498e9fc0ffaa84379a8d72a0f06
Ed Maste <emaste@FreeBSD.org>
2025-02-09 15:37:24 -0500

    ssh: Move XAUTH_PATH setting to ssh.mk
    XAUTH_PATH is normally set (in the upstream build infrastructure) in
    config.h.  We previously set it in ssh and sshd's Makefiles if LOCALBASE
    is set, and over time have occasionally defined it in config.h.
    
    Leave it unset in config.h and move the CFLAGS logic to to ssh.mk so
    that it will be set when building all ssh libraries and programs but
    still be set by LOCALBASE.
    
    Reviewed by:	jlduran
    Sponsored by:	The FreeBSD Foundation
    Differential Revision: https://reviews.freebsd.org/D48907
    
    (cherry picked from commit 7b8f4cc96a542b727ae4c54c709e099bdeaee705)

--- secure/ssh.mk
+++ secure/ssh.mk
@@ -20,6 +20,8 @@ CFLAGS+= -DHAVE_LDNS=1
 CFLAGS+= -DLIBWRAP=1
 .endif
 
+CFLAGS+= -DXAUTH_PATH=\"${LOCALBASE:U/usr/local}/bin/xauth\"
+
 .if ${MK_USB} != "no"
 # Built-in security key support
 CFLAGS+= -include sk_config.h
