2b391551f56bc4ce89a65d6da74499dc96ab8e73
Ed Maste <emaste@FreeBSD.org>
2025-10-15 16:57:32 -0400

    ssh: update makefiles for 10.1p1

--- secure/ssh.mk
+++ secure/ssh.mk
@@ -5,6 +5,9 @@
 SSHDIR=		${SRCTOP}/crypto/openssh
 
 SFTP_CLIENT_SRCS=sftp-common.c sftp-client.c sftp-glob.c
+
+P11SRCS=	ssh-pkcs11-client.c
+
 SKSRCS=	ssh-sk-client.c
 
 CFLAGS+= -I${SSHDIR} -I${SSHDIR}/openbsd-compat/include -include ssh_namespace.h
