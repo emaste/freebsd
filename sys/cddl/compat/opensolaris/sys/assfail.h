179f1d36d560caaccebfc56d48373fa7890fbc2e
Ed Maste <emaste@FreeBSD.org>
2026-09-17 12:50:31 -0400

    Revert "XXX remove more solaris stuff"
    This reverts commit 20047a17ab95ae45360a24dc912cb14795da3acf.

--- sys/cddl/compat/opensolaris/sys/assfail.h
+++ sys/cddl/compat/opensolaris/sys/assfail.h
@@ -38,6 +38,8 @@ extern "C" {
 #endif
 
 #ifdef _KERNEL
+void assfail3(const char *, uintmax_t, const char *, uintmax_t, const char *,
+    int);
 #else	/* !defined(_KERNEL) */
 
 #ifndef HAVE_ASSFAIL
