ea2869e34c476e1daa5c2339417aafa7d6722475
Quentin Thébault <quentin.thebault@defenso.fr>
2026-09-25 10:51:19 -0400

    vt: Fix timer race between vtterm_splash() and vt_flush()
    Current code leads to console text being drawn over the splash image.
    
    Ensure vt_flush() does not draw over the splash image: vtterm_splash()
    holds the vd_flush_lock via VT_FLUSH_LOCK(), or the vtbuf lock,
    depending on the framebuffer backend, while it sets VDF_SPLASH and
    blits. vt_flush() takes the same lock and re-checks VDF_SPLASH.
    
    Sponsored by:	Defenso
    Signed-off-by:	Quentin Thébault <quentin.thebault@defenso.fr>
    
    Differential Revision:	https://reviews.freebsd.org/D59928

--- sys/dev/vt/vt_core.c
+++ sys/dev/vt/vt_core.c
@@ -1533,7 +1533,7 @@ vt_flush(struct vt_device *vd)
 	if (vw == NULL)
 		return (0);
 
-	if (vd->vd_flags & VDF_SPLASH || vw->vw_flags & VWF_BUSY)
+	if (vw->vw_flags & VWF_BUSY)
 		return (0);
 
 	vf = vw->vw_font;
@@ -1541,8 +1541,14 @@ vt_flush(struct vt_device *vd)
 		return (0);
 
 	VT_FLUSH_LOCK(vd);
-
 	vtbuf_lock(&vw->vw_buf);
+
+	if (vd->vd_flags & VDF_SPLASH) {
+		vtbuf_unlock(&vw->vw_buf);
+		VT_FLUSH_UNLOCK(vd);
+		return (0);
+	}
+
 	inside_vt_flush = true;
 
 #ifndef SC_NO_CUTPASTE
@@ -1729,17 +1735,33 @@ vtterm_splash(struct vt_device *vd)
 
 	si = MD_FETCH(preload_kmdp, rebooting == 1 ? MODINFOMD_SHTDWNSPLASH :
 	    MODINFOMD_SPLASH, struct splash_info *);
+	/* Quit before taking the lock if the backend lacks something. */
 	if (si == NULL) {
 		if (vd->vd_driver->vd_bitblt_bmp == NULL)
 			return;
-	} else if (vd->vd_driver->vd_bitblt_argb == NULL)
+	} else if (vd->vd_driver->vd_bitblt_argb == NULL || si->si_depth != 4)
+		return;
+	if (rebooting == 1 && vd->vd_driver->vd_blank == NULL)
 		return;
 
-	if (rebooting == 1) {
-		if (vd->vd_driver->vd_blank == NULL)
-			return;
+	/*
+	 * VT_FLUSH_LOCK() locks vd_flush_lock only for backends with
+	 * vd_bitblt_after_vtbuf_unlock.
+	 */
+	if (vd->vd_driver->vd_bitblt_after_vtbuf_unlock)
+		mtx_lock(&vd->vd_flush_lock);
+	else
+		vtbuf_lock(&vd->vd_curwindow->vw_buf);
+
+	vd->vd_flags |= VDF_SPLASH;
+
+	if (vd->vd_driver->vd_bitblt_after_vtbuf_unlock)
+		mtx_unlock(&vd->vd_flush_lock);
+	else
+		vtbuf_unlock(&vd->vd_curwindow->vw_buf);
+
+	if (rebooting == 1)
 		vd->vd_driver->vd_blank(vd, TC_BLACK);
-	}
 
 	if (si == NULL) {
 		top = (vd->vd_height - vt_logo_height) / 2;
@@ -1748,8 +1770,6 @@ vtterm_splash(struct vt_device *vd)
 		    vd->vd_curwindow, vt_logo_image, NULL, vt_logo_width,
 		    vt_logo_height, left, top, TC_WHITE, TC_BLACK);
 	} else {
-		if (si->si_depth != 4)
-			return;
 		image = (uintptr_t)si + sizeof(struct splash_info);
 		image = roundup2(image, 8);
 		top = (vd->vd_height - si->si_height) / 2;
@@ -1758,7 +1778,6 @@ vtterm_splash(struct vt_device *vd)
 		    (unsigned char *)image, si->si_width, si->si_height,
 		    left, top);
 	}
-	vd->vd_flags |= VDF_SPLASH;
 }
 #endif
 
